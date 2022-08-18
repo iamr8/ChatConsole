d43faf3cf4f67ce5d66d89e4845817356e60acdd
Arash Shabbeh <arash.shabbeh@gmail.com>
2022-08-18 14:14:24 +0300

    Added documentation for `SocketMessageEventArgs`

--- ChatService.Base/SocketMessageEventArgs.cs
+++ ChatService.Base/SocketMessageEventArgs.cs
@@ -2,5 +2,8 @@
 
 public class SocketMessageEventArgs : EventArgs
 {
+    /// <summary>
+    /// The message that is received through the socket.
+    /// </summary>
     public string Message { get; init; }
 }
