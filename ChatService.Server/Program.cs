db1f8bda805f9ac4a650bb81f8e2bed70a9f3a30
Arash Shabbeh <arash.shabbeh@gmail.com>
2022-08-18 14:04:18 +0300

    ADDED a inital entry point for each side of the Chat

---
+++ ChatService.Server/Program.cs
@@ -0,0 +1,14 @@
+﻿using ChatService.Base;
+
+using var cancellationTokenSource = new CancellationTokenSource();
+using var socket = await SocketServer.CreateConnectionAsync("localhost", cancellationTokenSource.Token);
+await socket.ConnectAsync(cancellationTokenSource.Token);
+await socket.SendAsync("Server> Hello, I am your assist. How can I help you?", cancellationTokenSource.Token);
+socket.OnReceived += (sender, eventArgs) =>
+{
+    Console.WriteLine("Client> {0}", eventArgs.Message);
+};
+
+var line = Console.ReadLine();
+if (!string.IsNullOrEmpty(line))
+    await socket.SendAsync(line, cancellationTokenSource.Token);
