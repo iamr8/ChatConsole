a3906461d74fe9db1cf30839df35c8771c5a5449
Arash Shabbeh <arash.shabbeh@gmail.com>
2022-08-18 18:30:10 +0300

    updated

--- ChatService.Client/Program.cs
+++ ChatService.Client/Program.cs
@@ -1,13 +1,23 @@
 ﻿using ChatService.Base;
 
 using var cancellationTokenSource = new CancellationTokenSource();
-using var socket = await SocketClient.CreateConnectionAsync("localhost", cancellationTokenSource.Token);
-await socket.ConnectAsync(cancellationTokenSource.Token);
-socket.OnReceived += (sender, eventArgs) =>
-{
-    Console.WriteLine("Server> {0}", eventArgs.Message);
-};
 
-var line = Console.ReadLine();
-if (!string.IsNullOrEmpty(line))
-    await socket.SendAsync(line, cancellationTokenSource.Token);
+Console.WriteLine("Please enter an alias to connect to an assist.");
+var name = Console.ReadLine();
+
+using var socket = await SocketClient.CreateConnectionAsync("localhost", cancellationTokenSource.Token);
+socket.Connect(name);
+//await socket.ConnectAsync(cancellationTokenSource.Token);
+//socket.OnReceived += (sender, eventArgs) =>
+//{
+//    Console.WriteLine("Server> {0}", eventArgs.Message);
+//};
+
+//var line = Console.ReadLine();
+//if (!string.IsNullOrEmpty(line))
+//    await socket.SendAsync(line, cancellationTokenSource.Token);
+
+var message = Console.ReadLine();
+socket.Send(message);
+
+var f = Console.ReadKey();
--- ChatService.Server/Program.cs
+++ ChatService.Server/Program.cs
@@ -2,13 +2,16 @@
 
 using var cancellationTokenSource = new CancellationTokenSource();
 using var socket = await SocketServer.CreateConnectionAsync("localhost", cancellationTokenSource.Token);
-await socket.ConnectAsync(cancellationTokenSource.Token);
-await socket.SendAsync("Server> Hello, I am your assist. How can I help you?", cancellationTokenSource.Token);
-socket.OnReceived += (sender, eventArgs) =>
-{
-    Console.WriteLine("Client> {0}", eventArgs.Message);
-};
+socket.StartListening();
+//await socket.Connec7tAsync(cancellationTokenSource.Token);
+//await socket.SendAsync("Server> Hello, I am your assist. How can I help you?", cancellationTokenSource.Token);
+//socket.OnReceived += (sender, eventArgs) =>
+//{
+//    Console.WriteLine("Client> {0}", eventArgs.Message);
+//};
 
-var line = Console.ReadLine();
-if (!string.IsNullOrEmpty(line))
-    await socket.SendAsync(line, cancellationTokenSource.Token);
+//var line = Console.ReadLine();
+//if (!string.IsNullOrEmpty(line))
+//    await socket.SendAsync(line, cancellationTokenSource.Token);
+
+var f = Console.ReadKey();
