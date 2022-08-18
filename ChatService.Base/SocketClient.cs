db6d8ca5a6cd4ef17a37d1aff13c8de9e415f83e
Arash Shabbeh <arash.shabbeh@gmail.com>
2022-08-18 14:03:57 +0300

    Created two separated `SocketServer` and `SocketClient` classes, and make them derived from `SocketBase`

---
+++ ChatService.Base/SocketClient.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Text;
+
+namespace ChatService.Base;
+
+public record SocketClient : SocketBase
+{
+    public override event OnReceivedDelegate OnReceived;
+    private SocketClient(IPEndPoint endpoint) : base(endpoint)
+    {
+    }
+
+    public static async Task<SocketClient> CreateConnectionAsync(string host, CancellationToken cancellationToken = default)
+    {
+        var endPoint = await GetEndPointAsync(host, cancellationToken);
+        return new SocketClient(endPoint);
+    }
+
+    public override async Task ConnectAsync(CancellationToken cancellationToken = default)
+    {
+        string data = null;
+        var bytes = new byte[Sender.ReceiveBufferSize];
+
+        try
+        {
+            while (true)
+            {
+                var bytesReceived = Sender.Receive(bytes);
+                data += Encoding.ASCII.GetString(bytes, 0, bytesReceived);
+
+                if (data.IndexOf("-e") > -1)
+                    break;
+            }
+
+            OnReceived?.Invoke(this, new SocketMessageEventArgs { Message = data });
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    public override async Task SendAsync(string text, CancellationToken cancellationToken = default)
+    {
+        var msg = Encoding.UTF8.GetBytes(text);
+        Sender.Send(msg);
+    }
+}
---
+++ ChatService.Base/SocketServer.cs
@@ -0,0 +1,71 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatService.Base;
+
+public record SocketServer : SocketBase
+{
+    public override event OnReceivedDelegate OnReceived;
+    private Socket Client { get; set; }
+
+    private SocketServer(IPEndPoint endpoint) : base(endpoint)
+    {
+        Sender.Bind(_endpoint);
+        Sender.Listen(10);
+
+        Console.WriteLine("Waiting for a client...");
+    }
+
+    public static async Task<SocketServer> CreateConnectionAsync(string host, CancellationToken cancellationToken = default)
+    {
+        var endPoint = await GetEndPointAsync(host, cancellationToken);
+        return new SocketServer(endPoint);
+    }
+    private void CallAccept(IAsyncResult iar)
+    {
+        var server = (Socket)iar.AsyncState;
+        var client = server.EndAccept(iar);
+    }
+
+    public override async Task ConnectAsync(CancellationToken cancellationToken = default)
+    {
+        Client = await Sender.AcceptAsync(cancellationToken);
+
+        Console.WriteLine("Client joined.");
+
+        string data = null;
+        var bytes = new byte[Client.ReceiveBufferSize];
+
+        try
+        {
+            while (true)
+            {
+                var bytesReceived = Client.Receive(bytes);
+                data += Encoding.ASCII.GetString(bytes, 0, bytesReceived);
+
+                if (data.IndexOf("-e") > -1)
+                    break;
+            }
+
+            OnReceived?.Invoke(this, new SocketMessageEventArgs { Message = data });
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    public override async Task SendAsync(string text, CancellationToken cancellationToken = default)
+    {
+        var msg = Encoding.UTF8.GetBytes(text);
+        Client.Send(msg);
+    }
+
+    public override void Dispose()
+    {
+        Client.Shutdown(SocketShutdown.Both);
+        Client.Close();
+        Client.Dispose();
+    }
+}
