abf2364893f3b7cd6cd8977b15c9efb5916ef1f0
Arash Shabbeh <arash.shabbeh@gmail.com>
2022-08-18 18:29:33 +0300

    A `state` management for socket

---
+++ ChatService.Base/SocketState.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Sockets;
+
+namespace ChatService.Base;
+
+internal class SocketState
+{
+    public byte[] Buffer { get; set; }
+    public Socket Handler { get; set; }
+    public int BufferSize { get; set; }
+    public Dictionary<string, string> Bag { get; set; } = new();
+}
