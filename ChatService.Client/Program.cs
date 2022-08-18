129d2c3e6ca9d940e4e78163cc07f974775ae39f
Arash Shabbeh <arash.shabbeh@gmail.com>
2022-08-18 21:06:07 +0300

    updated consoles

--- ChatService.Client/Program.cs
+++ ChatService.Client/Program.cs
@@ -7,17 +7,8 @@ var name = Console.ReadLine();
 
 using var socket = await SocketClient.CreateConnectionAsync("localhost", cancellationTokenSource.Token);
 socket.Connect(name);
-//await socket.ConnectAsync(cancellationTokenSource.Token);
-//socket.OnReceived += (sender, eventArgs) =>
-//{
-//    Console.WriteLine("Server> {0}", eventArgs.Message);
-//};
-
-//var line = Console.ReadLine();
-//if (!string.IsNullOrEmpty(line))
-//    await socket.SendAsync(line, cancellationTokenSource.Token);
 
 var message = Console.ReadLine();
 socket.Send(message);
 
-var f = Console.ReadKey();
+Console.ReadKey();
--- ChatService.Server/Program.cs
+++ ChatService.Server/Program.cs
@@ -1,17 +1,11 @@
 ﻿using ChatService.Base;
 
 using var cancellationTokenSource = new CancellationTokenSource();
+
 using var socket = await SocketServer.CreateConnectionAsync("localhost", cancellationTokenSource.Token);
 socket.Start("Assist");
-//await socket.Connec7tAsync(cancellationTokenSource.Token);
-//await socket.SendAsync("Server> Hello, I am your assist. How can I help you?", cancellationTokenSource.Token);
-//socket.OnReceived += (sender, eventArgs) =>
-//{
-//    Console.WriteLine("Client> {0}", eventArgs.Message);
-//};
 
-//var line = Console.ReadLine();
-//if (!string.IsNullOrEmpty(line))
-//    await socket.SendAsync(line, cancellationTokenSource.Token);
+var message = Console.ReadLine();
+socket.Send(message);
 
-var f = Console.ReadKey();
+Console.ReadKey();
